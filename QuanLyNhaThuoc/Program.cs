addde7ecb8c874b4e91df14a4f7d6ca4fa997c09
kkhoautc <142975911+kkhoautc@users.noreply.github.com>
2025-11-01 23:42:00 +0700

    Update Program.cs

--- QuanLyNhaThuoc/Program.cs
+++ QuanLyNhaThuoc/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using QuanLyNhaThuoc.DanhMuc;
 namespace QuanLyNhaThuoc
 {
     internal static class Program
@@ -16,7 +16,7 @@ namespace QuanLyNhaThuoc
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmDangNhap());
         }
     }
 }
